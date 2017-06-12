4673355a1f88273deeab8b2befaf99ec4ab3adfe
newraina <newraina@users.noreply.github.com>
2017-06-12 13:50:29 +0800

    fix moment.js locale file ignore

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -367,7 +367,9 @@ module.exports = {
     // solution that requires the user to opt into importing specific locales.
     // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
     // You can remove this if you don't use Moment.js:
-    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    // new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    // 产品基本只面向中文用户, 因此默认包含 zh-cn 的 locale 文件, 而不是需要在文件中手动引入
+    new webpack.ContextReplacementPlugin(/moment[\\\/]locale$/, /^\.\/(zh-cn)$/),
   ],
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
